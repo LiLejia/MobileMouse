0a138aadf6dd85815ace333d42bebeffef3a2ea1
Henry Lee <leelejia@gmail.com>
2012-09-28 14:23:07 +0800

    Initial Commit

---
+++ MobileMouse/LJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LJAppDelegate.h
+//  MobileMouse
+//
+//  Created by Henry Lee on 12-9-28.
+//  Copyright (c) 2012年 Henry Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MobileMouse/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MobileMouse
+//
+//  Created by Henry Lee on 12-9-28.
+//  Copyright (c) 2012年 Henry Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LJAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LJAppDelegate class]));
+    }
+}
